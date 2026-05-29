8a701f0a1e9ed8166426d8979bbc80a515c48b6a
Arturo S. <112110733+ArthurxG@users.noreply.github.com>
2026-05-29 21:05:28 +0200

    feat: modificar descripcion opcion 4 rama-b

--- src/app.tsx
+++ src/app.tsx
@@ -33,7 +33,7 @@ const OPTIONS: Option[] = [
   {
     id: 4,
     title: "Opción 4",
-    description: "Pull Requests", 
+    description: "Ramas y Conflictos", 
     message: "Aprenderás a proponer y revisar cambios mediante Pull Requests.",
     featureFlag: false,
   },
